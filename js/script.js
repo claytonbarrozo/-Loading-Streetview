6c56f447f9fe944b78b5bab8896f8f9f6ca0ec1a
Clayton de Oliveira <clayton.barrozo@gmail.com>
2017-01-20 11:45:30 +0000

    .

--- js/script.js
+++ js/script.js
@@ -32,15 +32,16 @@ function loadData() {
     // NYTimes AJAX request goes here
     var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q='+ cityStr + '&sort=newest&api-key=16298ba2aec24a4fb62ba3ab75ccf0ba';
         
-        $.getJSON(nytimesUrl, function (data){
+        $.getJSON(nytimesUrl, function(data){
+
         $nytHeaderElem.text('New York Times Articles About' + cityStr);
 
-        articles = data.response.docs;
-            for(var i = 0; i < articles.length; i++){
+         articles = data.response.docs;
+            for (var i = 0; i < articles.length; i++) {
             var article = articles[i];
             $nytElem.append('<li class="article">' + 
                 '<a href ="'+article.web_url+'">'+article.headline.main+
-                '</a>'+'<p>' + article.snippet + '</p>' +
+                '</a>' + '<p>' + article.snippet + '</p>' +
                  '</li>');
             }
     });
@@ -48,6 +49,7 @@ function loadData() {
 
 
 }
+
 $('#form-container').submit(loadData);
 
 return false;
